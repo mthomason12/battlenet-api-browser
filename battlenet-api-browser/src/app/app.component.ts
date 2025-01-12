57171f2d3f7567419d73c5429ea6b0510294252e
M Thomason <matt@warpedcore.net>
2025-01-12 20:22:07 +0000

    Add apiclientservice and userdataservice

--- battlenet-api-browser/src/app/app.component.ts
+++ battlenet-api-browser/src/app/app.component.ts
@@ -6,13 +6,27 @@ import { MatButtonModule} from '@angular/material/button';
 import { MatSidenavModule, MatDrawerMode } from '@angular/material/sidenav';
 import { MatListModule } from '@angular/material/list';
 import { ApitreeComponent } from "../apitree/apitree.component";
+import { ApiclientService } from '../apiclient/apiclient.service';
+import { UserdataService } from '../userdata/userdata.service';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule, ApitreeComponent, RouterLink],
+  imports: [RouterOutlet, MatToolbarModule, MatIconModule, MatButtonModule, MatSidenavModule, MatListModule, 
+    ApitreeComponent, RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'battlenet-api-browser';
+  apiClient!: ApiclientService;
+
+  constructor(private apiCli: ApiclientService, private userdata: UserdataService)
+  {
+    this.apiClient = apiCli;
+  }
+
+  connect()
+  {
+    this.apiClient.connect("us", this.userdata.data.clientID, this.userdata.data.clientSecret);
+  }
 }
