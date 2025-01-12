7d35f86c16596c3ccac52e0c871d096120d3b59e
M Thomason <matt@warpedcore.net>
2025-01-12 20:21:49 +0000

    Add apiclientservice to config

--- battlenet-api-browser/src/app/app.config.ts
+++ battlenet-api-browser/src/app/app.config.ts
@@ -1,13 +1,15 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { UserdataService } from '../userdata/userdata.service';
+import { ApiclientService } from '../apiclient/apiclient.service';
 
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
+
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
     provideAnimationsAsync(), provideAnimationsAsync(),
-  {provide: UserdataService} 
+  {provide: UserdataService}, {provide: ApiclientService}
   ]
 };
