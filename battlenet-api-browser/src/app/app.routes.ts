2973e3b1dbf02847ba780e0dcd0ed722cd1cef02
M Thomason <matt@warpedcore.net>
2025-01-14 20:15:59 +0000

    Remove browse path

--- battlenet-api-browser/src/app/app.routes.ts
+++ battlenet-api-browser/src/app/app.routes.ts
@@ -1,11 +1,10 @@
 import { Routes } from '@angular/router';
 import { SettingsComponent } from '../settings/settings.component';
-import { BrowseComponent } from '../browse/browse.component';
 import { AuthComponent } from '../auth/auth.component';
+import { NullComponent } from '../null/null.component';
 
 export const routes: Routes = [
-    { path: '', component: BrowseComponent }, 
-    { path: 'browse', component: BrowseComponent },      
+    { path: '', component: NullComponent},   
     { path: 'settings', component: SettingsComponent },
     { path: 'auth', component: AuthComponent },
 ];
