d145695cc291b800cd8e21a069ed00e5cd5f9afa
M Thomason <matt@warpedcore.net>
2025-01-14 20:01:50 +0000

    A null component for when we don't want anything in the router slot

---
+++ battlenet-api-browser/src/null/null.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-null',
+  imports: [],
+  templateUrl: './null.component.html',
+  styleUrl: './null.component.scss'
+})
+export class NullComponent {
+
+}
