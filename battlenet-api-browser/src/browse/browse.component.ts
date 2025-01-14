9caea906913bab7f529d35a3f284278bbf0a6c3a
M Thomason <matt@warpedcore.net>
2025-01-14 21:30:45 +0000

    Conditional handlers

--- battlenet-api-browser/src/browse/browse.component.ts
+++ battlenet-api-browser/src/browse/browse.component.ts
@@ -1,13 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { MatCardModule } from '@angular/material/card';
 import { dataStruct } from '../userdata/userdata.service';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-browse',
-  imports: [ MatCardModule ],
+  imports: [ MatCardModule, MatButtonModule ],
   templateUrl: './browse.component.html',
   styleUrl: './browse.component.scss'
 })
 export class BrowseComponent {
   @Input() data?: dataStruct;
+
+  reloadData()
+  {
+    
+  }
 }
