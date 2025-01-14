7283e4ec35d6491d331d649c47836c383e670d96
M Thomason <matt@warpedcore.net>
2025-01-14 21:29:39 +0000

    No more need for nullcomponent

--- battlenet-api-browser/src/app/app.routes.ts
+++ battlenet-api-browser/src/app/app.routes.ts
@@ -1,10 +1,9 @@
 import { Routes } from '@angular/router';
 import { SettingsComponent } from '../settings/settings.component';
 import { AuthComponent } from '../auth/auth.component';
-import { NullComponent } from '../null/null.component';
 
 export const routes: Routes = [
-    { path: '', component: NullComponent},   
+    { path: '', children: []},   
     { path: 'settings', component: SettingsComponent },
     { path: 'auth', component: AuthComponent },
 ];
