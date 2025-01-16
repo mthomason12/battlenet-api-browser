8418afa0662bd4aeb4cf82e5262ef3b001255723
M Thomason <matt@warpedcore.net>
2025-01-16 10:59:08 +0000

    Begin working on nested routes

--- battlenet-api-browser/src/app/app.routes.ts
+++ battlenet-api-browser/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { SettingsComponent } from '../settings/settings.component';
 import { AuthComponent } from '../auth/auth.component';
 import { BrowseComponent } from '../browse/browse.component';
+import { AchievementsComponent } from '../browse/achievements/achievements.component';
 
 export const routes: Routes = [
     { path: 'browse', component: BrowseComponent, children: [
-        { path: '**', component: BrowseComponent }
+        { path: 'achievements', component: AchievementsComponent},
+        { path: '**', component: undefined }
     ]},     
     { path: 'settings', component: SettingsComponent },
     { path: 'auth', component: AuthComponent },
