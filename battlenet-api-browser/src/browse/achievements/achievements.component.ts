ccc3043a6dd276c9e5a21794cffe33ef93e814ea
M Thomason <matt@warpedcore.net>
2025-01-16 14:05:16 +0000

    Add link to achievement buttons

--- battlenet-api-browser/src/browse/achievements/achievements.component.ts
+++ battlenet-api-browser/src/browse/achievements/achievements.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { AbstractBrowseChildComponent } from '../abstract-browse-child/abstract-browse-child.component';
 import { MatButtonModule } from '@angular/material/button';
 import { achievementsDataDoc } from '../../userdata/userdata.service';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-achievements',
-  imports: [MatButtonModule],
+  imports: [MatButtonModule, RouterLink],
   templateUrl: './achievements.component.html',
   styleUrl: './achievements.component.scss'
 })
