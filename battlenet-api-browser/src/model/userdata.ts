16c6cd2086b4aeaa2557bbda15b6dc3936e13b55
M Thomason <matt@warpedcore.net>
2025-01-21 15:26:57 +0000

    Move userdata structs to userdata.ts

---
+++ battlenet-api-browser/src/model/userdata.ts
@@ -0,0 +1,70 @@
+import { dataStruct } from './datastructs';
+import { profileDataStruct } from './profile';
+import { publicDataStruct } from './gamedata';
+
+/**
+ * All API data retrieved and stored in the application
+ */
+export class apiDataStruct extends dataStruct
+{
+  wowpublic: publicDataStruct;
+  wowprofile: profileDataStruct;
+
+  /**
+   * Constructor
+   */
+  constructor()
+  {
+    super(undefined);
+    this.wowpublic = new publicDataStruct(this);
+    this.wowprofile = new profileDataStruct(this);
+  }
+
+  /**
+   * @inheritdoc
+   */  
+  override getName(): string
+  {
+    return "Battle.net API";
+  }
+
+  /**
+   * @inheritdoc
+   */
+  override children(): dataStruct[]
+  {
+    return super.children().concat([this.wowpublic, this.wowprofile]);
+  }
+
+  /**
+   * @inheritdoc
+   */
+  override myPath(): string {
+      return "browse";
+  }
+
+  //fix up any references after reloading from JSON
+  override postFixup()
+  {
+      this.wowpublic.fixup(this);
+      this.wowprofile.fixup(this);
+  }
+}
+
+/**
+ * Data for application API access - Client ID and Secret 
+ */
+class appKeyStruct
+{
+  clientID: string = "";
+  clientSecret: string = "";
+}
+
+/**
+ * All data held by {@link UserdataService}
+ */
+export class userDataStruct
+{
+  key: appKeyStruct = new appKeyStruct();
+  apiData: apiDataStruct = new apiDataStruct();
+}
--- battlenet-api-browser/src/userdata/userdata.service.ts
+++ battlenet-api-browser/src/userdata/userdata.service.ts
@@ -6,73 +6,8 @@ import _ from 'lodash';
 import { dataStruct } from '../model/datastructs';
 import { profileDataStruct } from '../model/profile';
 import { publicDataStruct } from '../model/gamedata';
+import { userDataStruct } from '../model/userdata';
 
-/**
- * All API data retrieved and stored in the application
- */
-class apiDataStruct extends dataStruct
-{
-  wowpublic: publicDataStruct;
-  wowprofile: profileDataStruct;
-
-  /**
-   * Constructor
-   */
-  constructor()
-  {
-    super(undefined);
-    this.wowpublic = new publicDataStruct(this);
-    this.wowprofile = new profileDataStruct(this);
-  }
-
-  /**
-   * @inheritdoc
-   */  
-  override getName(): string
-  {
-    return "Battle.net API";
-  }
-
-  /**
-   * @inheritdoc
-   */
-  override children(): dataStruct[]
-  {
-    return super.children().concat([this.wowpublic, this.wowprofile]);
-  }
-
-  /**
-   * @inheritdoc
-   */
-  override myPath(): string {
-      return "browse";
-  }
-
-  //fix up any references after reloading from JSON
-  override postFixup()
-  {
-      this.wowpublic.fixup(this);
-      this.wowprofile.fixup(this);
-  }
-}
-
-/**
- * Data for application API access - Client ID and Secret 
- */
-class appKeyStruct
-{
-  clientID: string = "";
-  clientSecret: string = "";
-}
-
-/**
- * All data held by {@link UserdataService}
- */
-class userDataStruct
-{
-  key: appKeyStruct = new appKeyStruct();
-  apiData: apiDataStruct = new apiDataStruct();
-}
 
 const dataItem: string = 'battlenet-api-data';
 
