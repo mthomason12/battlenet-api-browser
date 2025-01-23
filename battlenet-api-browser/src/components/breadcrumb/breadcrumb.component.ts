79d925b73613bf054c279a6b6f124c416a9491ab
M Thomason <matt@warpedcore.net>
2025-01-23 21:37:12 +0000

    Routerlink, not href!

--- battlenet-api-browser/src/components/breadcrumb/breadcrumb.component.ts
+++ battlenet-api-browser/src/components/breadcrumb/breadcrumb.component.ts
@@ -2,10 +2,11 @@ import { Component, input } from '@angular/core';
 import { dataStruct } from '../../model/datastructs';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-breadcrumb',
-  imports: [MatButtonModule, MatIconModule],
+  imports: [MatButtonModule, MatIconModule, RouterLink],
   templateUrl: './breadcrumb.component.html',
   styleUrl: './breadcrumb.component.scss'
 })
