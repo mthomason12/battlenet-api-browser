71f0c985553a70eef610ad4390714f64be3619a5
M Thomason <matt@warpedcore.net>
2025-01-24 14:35:04 +0000

    Abstract browse-detail component

---
+++ battlenet-api-browser/src/browse/abstract-browse-detail/abstract-browse-detail.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { UserdataService } from '../../userdata/userdata.service';
+import { dataDoc, dataDocDetailsCollection } from '../../model/datastructs';
+import { AbstractBrowseChildComponent } from '../abstract-browse-child/abstract-browse-child.component';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-abstract-browse-detail',
+  imports: [],
+  templateUrl: './abstract-browse-detail.component.html',
+  styleUrl: './abstract-browse-detail.component.scss'
+})
+export abstract class abstractBrowseDetailComponent <T1 extends dataDocDetailsCollection<T1,T2>, T2 extends dataDoc> extends AbstractBrowseChildComponent<T2>{
+  datadoc?: T2;
+  id?: string;
+
+  constructor(private route: ActivatedRoute, protected override data: UserdataService)
+  {
+    super(data);
+  }
+
+  //things to do just before oninit
+  override preinit()
+  {
+    this.id = this.route.snapshot.paramMap.get('id') ?? "";    
+    this.datadoc = this.currentMaster().details.find(
+      (data, index, array)=>{
+        return Number.parseInt(this.id!) == data.id;
+      }
+    )!; 
+  }
+
+  /**
+   * Override in descendant classes to return the master for this detail document
+   * e.g. return this.data.data.apiData.wowpublic.creatureFamiliesData
+   */
+  abstract currentMaster(): T1
+
+  override currentData(): T2
+  {
+    return this.datadoc!;
+  }
+
+  
+}
