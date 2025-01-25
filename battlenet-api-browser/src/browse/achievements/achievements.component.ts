3f9407641cc429b587c777e0df477efcf355a076
M Thomason <matt@warpedcore.net>
2025-01-25 11:44:41 +0000

    Use a virtual scroller to speed up the achievement list

--- battlenet-api-browser/src/browse/achievements/achievements.component.ts
+++ battlenet-api-browser/src/browse/achievements/achievements.component.ts
@@ -1,12 +1,13 @@
-import { Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AbstractBrowseChildComponent } from '../abstract-browse-child/abstract-browse-child.component';
 import { MatButtonModule } from '@angular/material/button';
+import { ScrollingModule } from '@angular/cdk/scrolling';
 import { achievementsDataDoc } from '../../model/achievements';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-achievements',
-  imports: [MatButtonModule, RouterLink],
+  imports: [MatButtonModule, ScrollingModule, RouterLink],
   templateUrl: './achievements.component.html',
   styleUrl: './achievements.component.scss'
 })
