78c345c7d0af7817f728da739b39945d1b9c3059
M Thomason <matt@warpedcore.net>
2025-01-25 12:57:50 +0000

    Use new cached image component

--- battlenet-api-browser/src/browse/covenant/covenant.component.ts
+++ battlenet-api-browser/src/browse/covenant/covenant.component.ts
@@ -6,10 +6,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 import { AbstractBrowseDetailComponent } from '../abstract-browse-detail/abstract-browse-detail.component';
+import { CachedImageComponent } from "../../components/cached-image/cached-image.component";
 
 @Component({
   selector: 'app-covenant',
-  imports: [ MatTabsModule, MatListModule, MatTableModule, MatButtonModule, RouterLink ],
+  imports: [MatTabsModule, MatListModule, MatTableModule, MatButtonModule, RouterLink, CachedImageComponent],
   templateUrl: './covenant.component.html',
   styleUrl: './covenant.component.scss'
 })
