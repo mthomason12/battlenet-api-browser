afdda00ff3ed93e5b1f8fd3c0e63fd653e69400b
M Thomason <matt@warpedcore.net>
2025-01-25 20:32:48 +0000

    Add retrieve function

--- battlenet-api-browser/src/components/cached-image/cached-file.service.ts
+++ battlenet-api-browser/src/components/cached-image/cached-file.service.ts
@@ -7,11 +7,19 @@ export class CachedFileService {
 
   constructor() { }
 
+  /** Store image in database */
   store(url: string, data: any)
   {
 
   }
 
+  /** Retrieve image from database */
+  retrieve(url: string, data: any)
+  {
+
+  }
+
+  /** Get an image.  From database if possible, but if it's not there fetch and retrieve it*/
   get(url: string, data: any)
   {
 
