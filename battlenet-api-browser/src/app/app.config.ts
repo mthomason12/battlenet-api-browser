94ce01dfbdc8c44315cfbabb590e72e2f26cc03e
M Thomason <matt@warpedcore.net>
2025-01-25 20:30:20 +0000

    Provide a CachedFileService

--- battlenet-api-browser/src/app/app.config.ts
+++ battlenet-api-browser/src/app/app.config.ts
@@ -2,6 +2,7 @@ import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { UserdataService } from '../services/userdata.service';
 import { ApiclientService } from '../apiclient/apiclient.service';
+import { CachedFileService } from '../components/cached-image/cached-file.service';
 import { provideHttpClient } from '@angular/common/http';
 import { provideOAuthClient } from 'angular-oauth2-oidc';
 
@@ -12,6 +13,6 @@ import { provideAnimationsAsync } from '@angular/platform-browser/animations/asy
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), 
     provideAnimationsAsync(), provideAnimationsAsync(), provideHttpClient(), provideOAuthClient(),
-  {provide: UserdataService}, {provide: ApiclientService}
+  {provide: UserdataService}, {provide: ApiclientService}, {provide: CachedFileService}
   ]
 };
