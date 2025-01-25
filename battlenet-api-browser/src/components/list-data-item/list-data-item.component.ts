efd43c8b1cabe6307c099bf801bf4024f8ae208f
M Thomason <matt@warpedcore.net>
2025-01-25 15:22:14 +0000

    Add icon to indicate if an entry has been loaded

--- battlenet-api-browser/src/components/list-data-item/list-data-item.component.ts
+++ battlenet-api-browser/src/components/list-data-item/list-data-item.component.ts
@@ -1,13 +1,24 @@
 import { Component, input } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { dataDoc } from '../../model/datastructs';
+import { dataDoc, dataDocDetailsCollection } from '../../model/datastructs';
+import { MatIcon } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-list-data-item',
-  imports: [RouterLink],
+  imports: [MatButtonModule, RouterLink, MatIcon],
   templateUrl: './list-data-item.component.html',
   styleUrl: './list-data-item.component.scss'
 })
 export class ListDataItemComponent {
   item = input.required<dataDoc>();
+
+  itemIsLoaded(): boolean
+  {
+    if (this.item()._parent instanceof dataDocDetailsCollection)
+    {
+      return (this.item()._parent as dataDocDetailsCollection<any,any>).hasDetailEntry(this.item().id);
+    }
+    return false;
+  }
 }
