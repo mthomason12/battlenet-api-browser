efddcbdc981832f1409aa0f509dcdd09e80eb152
M Thomason <matt@warpedcore.net>
2025-01-25 13:49:02 +0000

    Remove unused reference

--- battlenet-api-browser/src/browse/covenant/covenant.component.ts
+++ battlenet-api-browser/src/browse/covenant/covenant.component.ts
@@ -6,12 +6,11 @@ import { MatButtonModule } from '@angular/material/button';
 import { RouterLink } from '@angular/router';
 import { MatTableModule } from '@angular/material/table';
 import { AbstractBrowseDetailComponent } from '../abstract-browse-detail/abstract-browse-detail.component';
-import { CachedImageComponent } from "../../components/cached-image/cached-image.component";
 import { MediaTableComponent } from '../../components/media-table/media-table.component';
 
 @Component({
   selector: 'app-covenant',
-  imports: [MatTabsModule, MatListModule, MatTableModule, MatButtonModule, RouterLink, MediaTableComponent, CachedImageComponent],
+  imports: [MatTabsModule, MatListModule, MatTableModule, MatButtonModule, RouterLink, MediaTableComponent],
   templateUrl: './covenant.component.html',
   styleUrl: './covenant.component.scss'
 })
