c396a5862dbabada1fd4af722bc7c5f88de18f42
M Thomason <matt@warpedcore.net>
2025-01-30 05:36:20 +0000

    Fix sorting

--- battlenet-api-browser/src/browse/list-detail-host/character-table/character-table.component.ts
+++ battlenet-api-browser/src/browse/list-detail-host/character-table/character-table.component.ts
@@ -1,9 +1,19 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
-import { MatTableModule } from '@angular/material/table';
+import { Component, ViewChild } from '@angular/core';
+import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { AbstractMasterComponent } from '../abstract-master/abstract-master.component';
 import { charsDataDoc } from '../../../model/account-characters';
 import { MatSort, MatSortModule } from '@angular/material/sort';
-import { jsonIgnore } from 'json-ignore';
+
+interface charRow {
+  name: string;
+  level: number;
+  class: string;
+  race: string;
+  faction: string;
+  server: string;
+  account: number;
+}
+
 
 @Component({
   selector: 'app-character-table',
@@ -13,11 +23,32 @@ import { jsonIgnore } from 'json-ignore';
   inputs: ['data','clicked']
 })
 export class CharacterTableComponent extends AbstractMasterComponent<charsDataDoc>{
-  displayedColumns: string[] = ['name','level','class','race','faction','server'];
+  displayedColumns: string[] = ['name','level','class','race','faction','server','account'];
+
+  rows: charRow[] = new Array();
+  dataSource: MatTableDataSource<charRow> = new MatTableDataSource();
 
   @ViewChild(MatSort) sort?: MatSort;
 
   ngAfterViewInit() {
-    //this.dataSource.sort = this.sort;
+    this.dataSource.sort = this.sort!;
   }  
+
+  override dataSet(): void {
+    //data has been set, lets clone over what we need
+    this.dataSource.data.length=0;
+    this.data!.items.forEach((item)=>{
+      this.dataSource.data.push(
+        {
+          name: item.name,
+          level: item.level!,
+          class: item.playable_class!.name,
+          race: item.playable_race!.name,
+          faction: item.faction!.name,
+          server: item.realm!.name,
+          account: item.account!
+        }
+      )
+    });
+  }
 }
