12d2d6bdf80ba1a61d72a1885c3a232bc367754b
M Thomason <matt@warpedcore.net>
2025-01-30 14:28:49 +0000

    Add filtering by account

--- battlenet-api-browser/src/browse/list-detail-host/character-table/character-table.component.ts
+++ battlenet-api-browser/src/browse/list-detail-host/character-table/character-table.component.ts
@@ -3,6 +3,13 @@ import { MatTableDataSource, MatTableModule } from '@angular/material/table';
 import { AbstractMasterComponent } from '../abstract-master/abstract-master.component';
 import { charsDataDoc } from '../../../model/account-characters';
 import { MatSort, MatSortModule } from '@angular/material/sort';
+import { MatExpansionModule } from '@angular/material/expansion';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatSelectModule } from '@angular/material/select';
+import { FormControl, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import '../../../lib/utils';
 
 interface charRow {
   name: string;
@@ -17,7 +24,8 @@ interface charRow {
 
 @Component({
   selector: 'app-character-table',
-  imports: [ MatTableModule, MatSortModule ],
+  imports: [ MatTableModule, MatSortModule, MatExpansionModule, MatIconModule, MatFormFieldModule, 
+    MatInputModule, MatSelectModule, FormsModule, ReactiveFormsModule ],
   templateUrl: './character-table.component.html',
   styleUrl: './character-table.component.scss',
   inputs: ['data','clicked']
@@ -25,14 +33,27 @@ interface charRow {
 export class CharacterTableComponent extends AbstractMasterComponent<charsDataDoc>{
   displayedColumns: string[] = ['name','level','class','race','faction','server','account'];
 
+  //data
   rows: charRow[] = new Array();
+  accounts: number[] = new Array();
   dataSource: MatTableDataSource<charRow> = new MatTableDataSource();
+
+  //filters
+  accountFilter: number[] = new Array();
 
   @ViewChild(MatSort) sort?: MatSort;
 
   ngAfterViewInit() {
     this.dataSource.sort = this.sort!;
+    this.dataSource.filterPredicate = (data, filter)=>{
+      return ((this.accountFilter.length == 0) || this.accountFilter.includes(data.account) )
+    };
   }  
+
+  filterChanged()
+  {
+    this.dataSource.filter = this.accountFilter.toString();
+  }
 
   override dataSet(): void {
     //data has been set, lets clone over what we need
@@ -50,5 +71,9 @@ export class CharacterTableComponent extends AbstractMasterComponent<charsDataDo
         }
       )
     });
+    //get list of unique accounts
+    this.accounts = this.dataSource.data.map((value, index, array)=>{
+      return value.account;
+    }).onlyUnique();
   }
 }
