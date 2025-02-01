73b2fd7ca0e3721ec423161d488a70f289eb953a
M Thomason <matt@warpedcore.net>
2025-02-01 02:41:20 +0000

    Add Array.merge

--- battlenet-api-browser/src/lib/utils.ts
+++ battlenet-api-browser/src/lib/utils.ts
@@ -6,12 +6,26 @@ declare global {
   
   interface Array<T>
   {
-      onlyUnique(): any;
+      onlyUnique(): any[];
+      merge(arr: any[], key: string): any[]
   }
 }
 
+/**
+ * Return only unique values from the array
+ */
 Array.prototype.onlyUnique = function(): any[] {
     return this.filter((value, index, arr) => {return arr.indexOf(value) === index});
+}
+
+/**
+ * Merge a 2nd array over the current array, using key as the unique record identifier, and return the result
+ */
+Array.prototype.merge = function(arr: any[], key: string): any[] {
+  return Array.from([...this, ...arr]
+    .reduce((prev, current) => prev.set(current[key], current), new Map)
+    .values()
+);
 }
 
 
