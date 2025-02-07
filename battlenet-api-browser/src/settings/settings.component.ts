82cf5e4a375bb5217e75fda0bbeeef584c03b49e
M Thomason <matt@warpedcore.net>
2025-02-07 15:28:22 +0000

    New card-based layout for settings

--- battlenet-api-browser/src/settings/settings.component.ts
+++ battlenet-api-browser/src/settings/settings.component.ts
@@ -6,11 +6,15 @@ import { MatInputModule } from '@angular/material/input';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatExpansionModule, MatAccordion } from '@angular/material/expansion';
 import { MatCheckboxModule } from '@angular/material/checkbox';
+import { MatRadioModule } from '@angular/material/radio';
 import { settingsStruct } from '../model/userdata';
+import { MatCardModule } from '@angular/material/card';
+import _ from 'lodash';
 
 @Component({
   selector: 'app-settings',
-  imports: [ FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule, MatExpansionModule, MatAccordion, MatCheckboxModule ],
+  imports: [ FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule, 
+    MatExpansionModule, MatAccordion, MatCheckboxModule, MatCardModule, MatRadioModule ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
@@ -20,11 +24,11 @@ export class SettingsComponent {
   clientSecret: string;
   settings: settingsStruct = new settingsStruct()
 
-  constructor(private userdata: UserdataService)
+  constructor(protected userdata: UserdataService)
   {
     this.clientId = this.userdata.data.key.clientID;
     this.clientSecret = this.userdata.data.key.clientSecret;
-    this.settings = this.userdata.data.settings;
+    this.settings = _.merge(this.settings, this.userdata.data.settings)
   }
 
   reset()
