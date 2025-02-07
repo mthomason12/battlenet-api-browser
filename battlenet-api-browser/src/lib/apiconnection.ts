367ba1733c6b56cb8a8e157d56881dd3232f691b
M Thomason <matt@warpedcore.net>
2025-02-07 21:07:50 +0000

    Add abstract APIConnection class

---
+++ battlenet-api-browser/src/lib/apiconnection.ts
@@ -0,0 +1,27 @@
+
+/**
+ * New forms of API connection (e.g. via a proxy, an SSH tunnel, or an encrypted IP stream) should
+ * inherit from this.
+ */
+abstract class APIConnection {
+
+    /**
+     * Attempt to establish connection.  Return true if connection successfully 
+     * established, false if not.
+     */
+    abstract connect(): Promise<boolean>;
+
+    /**
+     * Attempt battle.net authentication.  Return true if successful, false if not.
+     */
+    abstract authenticate(): Promise<boolean>;
+
+    /**
+     * Execute API call
+     * @param apiEndpoint 
+     * @param params 
+     * @param options 
+     */
+    abstract apiCall<T = any>(apiEndpoint: string, params: string, options: object): Promise<T | undefined>
+
+}
