258513a9106ee4f2af8cbc56178c6b45ab366e5f
M Thomason <matt@warpedcore.net>
2025-02-11 14:03:48 +0000

    Add profileCharacters data

--- battlenet-api-browser/src/model/profile.ts
+++ battlenet-api-browser/src/model/profile.ts
@@ -1,14 +1,18 @@
 import { RecDB } from '../lib/recdb';
 import { dataStruct, topDataStruct } from './datastructs';
+import { profileCharactersDataDoc } from './profile-characters';
 
 export class profileDataStruct extends topDataStruct
 {
+  characterData: profileCharactersDataDoc;
 
   constructor(parent: dataStruct, recDB: RecDB)
   {
     super(parent, recDB);
   
-    this.addFolder("Characters");
+    this.addFolder("Characters", [
+      this.characterData = this.dbRegister(profileCharactersDataDoc)
+    ]);
 
     this.addFolder("Guilds");
 
