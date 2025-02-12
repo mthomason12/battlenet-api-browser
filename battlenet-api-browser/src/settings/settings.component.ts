4e8050cc2e0e16357e7dc8d66cd9c07c50eff837
M Thomason <matt@warpedcore.net>
2025-02-12 23:23:29 +0000

    Prepare to handle multiple tabs

--- battlenet-api-browser/src/settings/settings.component.ts
+++ battlenet-api-browser/src/settings/settings.component.ts
@@ -10,11 +10,16 @@ import { MatRadioModule } from '@angular/material/radio';
 import { appKeyStruct, settingsStruct } from '../model/userdata';
 import { MatCardModule } from '@angular/material/card';
 import _ from 'lodash';
+import { ExtensionManagerService } from '../extensions/extension-manager.service';
+import { apiClientService } from '../services/apiclient.service';
+import { CommonModule } from '@angular/common';
+import { MatTabsModule } from '@angular/material/tabs';
 
 @Component({
   selector: 'app-settings',
   imports: [ FormsModule, MatButtonModule, MatInputModule, MatFormFieldModule, 
-    MatExpansionModule, MatAccordion, MatCheckboxModule, MatCardModule, MatRadioModule ],
+    MatExpansionModule, MatAccordion, MatCheckboxModule, MatCardModule, MatRadioModule, 
+    MatTabsModule, CommonModule ],
   templateUrl: './settings.component.html',
   styleUrl: './settings.component.scss'
 })
@@ -24,8 +29,12 @@ export class SettingsComponent {
   settings = input.required<settingsStruct>();
   key = input.required<appKeyStruct>();
 
-  constructor(protected userdata: UserdataService)
+  constructor(protected userdata: UserdataService, protected extmgr: ExtensionManagerService, protected api: apiClientService)
   {
+  }
+
+  getConnections() {
+    return this.api.connections;
   }
 
 }
