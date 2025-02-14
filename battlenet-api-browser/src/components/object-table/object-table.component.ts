fc6557b85642e04b4ac1b76cdd61dd79f9f87603
M Thomason <matt@warpedcore.net>
2025-02-14 15:44:34 +0000

    Flatten object tables in ObjectTableComponent to reduce complexity, as  rendering many tables within tables is overloading browsers

--- battlenet-api-browser/src/components/object-table/object-table.component.ts
+++ battlenet-api-browser/src/components/object-table/object-table.component.ts
@@ -27,7 +27,7 @@ export class ObjectTableComponent implements OnInit {
     }
 
     rows(): propertyData[] {
-      return ObjToArray(this.dataSource()).filter((value)=>{return value.name !== '_parent'});
+      return ObjToArray(this.flattenObj(this.dataSource())).filter((value)=>{return value.name !== '_parent'});
     }
 
     objToArray(obj: any): propertyData[]
@@ -38,6 +38,42 @@ export class ObjectTableComponent implements OnInit {
     isObject(data: any): boolean {
       return isObject(data);
     }
+
+    isArray(data: any): boolean {
+      return Array.isArray(data);
+    }
+
+    flattenObj(ob: any): any {
+ 
+      // The object which contains the
+      // final result
+      let result:any = {};
+   
+      // loop through the object "ob"
+      for (const i in ob) {
+   
+          // We check the type of the i using
+          // typeof() function and recursively
+          // call the function again
+          if (Array.isArray(ob))
+          {
+            result[i] = this.flattenObj((ob as any)[i]);
+          } else if ((typeof ob[i]) === 'object') {
+              const temp = this.flattenObj(ob[i]);
+              for (const j in temp) {
+   
+                  // Store temp in result
+                  result[i + '.' + j] = temp[j];
+              }
+          } 
+   
+          // Else store ob[i] in result directly
+          else {
+              result[i] = ob[i];
+          }
+      }
+      return result;
+  };
   
 
 
