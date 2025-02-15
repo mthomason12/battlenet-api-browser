0fd3b3b9e08367c1c004db92313e38d4bcd9c5e5
M Thomason <matt@warpedcore.net>
2025-02-15 05:46:13 +0000

    Electron http handler

--- battlenet-api-browser/main.js
+++ battlenet-api-browser/main.js
@@ -1,26 +1,52 @@
-const { app, BrowserWindow } = require('electron/main')
+const { app, BrowserWindow, protocol } = require('electron/main')
+
+
+let win;
+
+const rootFile = 'dist/battlenet-api-browser/browser/index.html';
 
 const createWindow = () => {
-  const win = new BrowserWindow({
+  win = new BrowserWindow({
     width: 800,
     height: 600
   })
 
-  win.loadFile('dist/battlenet-api-browser/browser/index.html')
+  win.loadFile(rootFile);
+
 }
 
-app.whenReady().then(() => {
-  createWindow()
-
-  app.on('activate', () => {
-    if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow()
-    }
-  })
-})
+/*
+app.on('open-url', (event, url) => {
+  if (!url.startsWith("http://localhost:4200/auth-callback")) return;
+  event.preventDefault();
+  win.loadFile(rootFile);
+});*/
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') {
     app.quit()
+  } 
+});
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
   }
 })
+
+app.whenReady().then(() => {
+  createWindow();
+
+  protocol.handle('http', (request) => {
+    return new Response('<h1>Redirecting...</h1>',{
+      headers: { 
+        'content-type': 'text/html',
+        'location': url.pathToFileURL(path.join(process.resourcesPath, 'app.asar', rootfile)).toString()
+      },
+      status: 301
+    });
+  });
+})
+
+
+
