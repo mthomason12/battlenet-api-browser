9a13fa291d32a36d022327a905ccbf4d41754e24
M Thomason <matt@warpedcore.net>
2025-02-15 12:44:01 +0000

    Change auth-callback for Electron to the root

--- battlenet-api-browser/src/services/blizzardapi-connection.ts
+++ battlenet-api-browser/src/services/blizzardapi-connection.ts
@@ -63,7 +63,7 @@ export class BlizzardAPIConnection extends APIConnection {
 
     getRedirectURI(): string {
         if (isElectron()) {
-            return 'http://localhost:4200/auth-callback'
+            return 'http://localhost:4200/?auth-callback=true'
         }
         else
             return window.location.origin+'/auth-callback';
