f6581b620e076ed7fffe66996957144bdbf17d4c
M Thomason <matt@warpedcore.net>
2025-02-15 11:39:17 +0000

    Ruggedize refresh()

--- battlenet-api-browser/src/browse/list-detail-host/list-detail-host.component.ts
+++ battlenet-api-browser/src/browse/list-detail-host/list-detail-host.component.ts
@@ -113,19 +113,25 @@ export class ListDetailHostComponent implements OnInit, OnDestroy {
   {
     if (this.mode == this.Mode.Master)
     {
-      this.masterList!.reload(this.apiClient).then((list)=>{
-        this.userData.dataRefreshedEmitter.emit();
-      });
+      if (this.masterList?.canReload())
+      {
+        this.masterList?.reload(this.apiClient).then((list)=>{
+          this.userData.dataRefreshedEmitter.emit();
+        });
+      }
     }
     else
     {
-      this.masterList!.reloadItem(this.apiClient, this.id).then((rec)=>{
-        this.detailItem = rec;
-        this.userData.setCurrent(this.masterList!, this.detailItem!);
-        this.mode = ListDetailHostComponentMode.Detail;
-        this.detailInputs = { 'data': this.detailItem! };               
-        this.userData.dataRefreshedEmitter.emit();
-      });
+      if (this.masterList?.canReloadItems())
+      {
+        this.masterList?.reloadItem(this.apiClient, this.id).then((rec)=>{
+          this.detailItem = rec;
+          this.userData.setCurrent(this.masterList!, this.detailItem!);
+          this.mode = ListDetailHostComponentMode.Detail;
+          this.detailInputs = { 'data': this.detailItem! };               
+          this.userData.dataRefreshedEmitter.emit();
+        });
+      }
     }
   }
 
