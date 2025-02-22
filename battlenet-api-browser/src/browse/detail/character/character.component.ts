f369cf1d3e7723a5e071631c613d688cdc9b19fe
M Thomason <matt@warpedcore.net>
2025-02-22 00:22:38 +0000

    Split summary boxes better

--- battlenet-api-browser/src/browse/detail/character/character.component.ts
+++ battlenet-api-browser/src/browse/detail/character/character.component.ts
@@ -18,9 +18,11 @@ import { PageSelectorComponent } from '../../../components/page-selector/page-se
 export class CharacterComponent extends AbstractDetailComponent<characterProfileData> {
 
   overviewData: IKeyValueTableData[] = [];
+  pointsSummary: IKeyValueTableData[] = [];
+  shadowlandsSummary: IKeyValueTableData[] = [];
   professionSummary: IKeyValueTableData[] = [];
   collectionSummary: IKeyValueTableData[] = [];
-  statsData: IKeyValueTableData[] = [];
+  statsSummary: IKeyValueTableData[] = [];
   achievementData: characterAchievement[] = [];
   statisticsData: characterAchievementStatisticsCategory[] = [];
 
@@ -30,14 +32,19 @@ export class CharacterComponent extends AbstractDetailComponent<characterProfile
       { key: 'ID', value: this.data?.id! },
       { key: 'Faction', value: this.data?.faction.name! },
       { key: 'Realm', value: this.data?.realm.name! },
+      { key: 'Guild', value: this.data?.guild.name!},
       { key: 'Class', value: this.data?.character_class.name! },      
+      { key: 'Level', value: this.data?.level! },
       { key: 'Active Spec', value: this.data?.active_spec.name!, button: {
         name: "Other Specs", onClick: ()=>{}
       }},
       ...(this.data?.$hunterPets?.hunter_pets.length! > 0) ? [
         { key: 'Hunter Pets', value: this.data?.$hunterPets.hunter_pets.length! , button: {
           name: "Hunter Pets", onClick: ()=>{}
-        }}] : [],      
+      }}] : [],  
+    ];
+    this.pointsSummary =[
+      { key: 'Average Item Level', value: this.data?.equipped_item_level! },        
       { key: 'Equipped Item Level', value: this.data?.equipped_item_level!, button: {
         name: "Equipment", onClick: ()=>{}
       }},                          
@@ -46,19 +53,15 @@ export class CharacterComponent extends AbstractDetailComponent<characterProfile
       }},     
       { key: 'Achievement Points', value: this.data?.achievement_points!, button: {
         name: "Achievements", onClick: ()=>{}
-      }},
+      }}];
+    this.shadowlandsSummary = [
+      ...(this.data?.covenant_progress?.chosen_covenant) ? [
+        {key: 'Covenant', value: this.data?.covenant_progress?.chosen_covenant.name }
+      ] : [],
       ...(this.data?.$soulbinds?.soulbinds?.length! > 0) ? [
         { key: 'Soulbinds', value: this.data?.$soulbinds?.soulbinds?.length! , button: {
           name: "Soulbinds", onClick: ()=>{}
         }}] : [],     
-    ];
-    this.professionSummary = [
-      ...this.data?.$professions.primaries.map((item)=>{ return { key: item.profession.name!, value: this.profSkillTotal(item), button: {
-        name: item.profession.name!, onClick: ()=>{}
-      }}; })! as IKeyValueTableData[],
-      ...this.data?.$professions.secondaries.map((item)=>{ return { key: item.profession.name!, value: this.profSkillTotal(item), button: {
-        name: item.profession.name!, onClick: ()=>{}
-      }}; })! as IKeyValueTableData[],
     ];
     this.collectionSummary = [
       { key: 'Heirlooms:', value: this.data?.$heirlooms?.heirlooms?.length! , button: {
@@ -80,7 +83,18 @@ export class CharacterComponent extends AbstractDetailComponent<characterProfile
         name: "Transmog Pieces", onClick: ()=>{}
       }},
     ]
-    this.statsData = [
+    this.professionSummary = [
+      ...this.data?.$professions.primaries.map((item)=>{ return { key: item.profession.name!, value: this.profSkillTotal(item), button: {
+        name: item.profession.name!, onClick: ()=>{}
+      }}; })! as IKeyValueTableData[],
+      ...this.data?.$professions.secondaries.map((item)=>{ return { key: item.profession.name!, value: this.profSkillTotal(item), button: {
+        name: item.profession.name!, onClick: ()=>{}
+      }}; })! as IKeyValueTableData[],
+    ];
+    this.statsSummary = [
+      { key: 'Health', value: this.data?.$statistics.health!},
+      { key: 'Power', value: this.data?.$statistics.power!},
+      { key: 'Power Type', value: this.data?.$statistics.power_type.name!},
       { key: 'Strength (base)', value: this.data?.$statistics.strength.base!},
       { key: 'Strength (effective)', value: this.data?.$statistics.strength.effective!},       
       { key: 'Agility (base)', value: this.data?.$statistics.agility.base!},
@@ -89,6 +103,8 @@ export class CharacterComponent extends AbstractDetailComponent<characterProfile
       { key: 'Intellect (effective)', value: this.data?.$statistics.intellect.effective!},
       { key: 'Stamina (base)', value: this.data?.$statistics.stamina.base!},
       { key: 'Stamina (effective)', value: this.data?.$statistics.stamina.effective!},
+      { key: 'Speed', value: this.data?.$statistics.speed.rating!},
+      { key: 'Speed Bonus', value: this.data?.$statistics.speed.rating_bonus!},
     ]
     this.achievementData = this.data?.$achievements?.achievements!;
     this.statisticsData = this.data?.$achievementStatistics?.categories!;
